27643f56ba3c41c22790259a9490274907418bff
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2022-12-11 21:17:28 +0000

    changed connection

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,7 +1,27 @@
+// const mongoose = require('mongoose')
+
+// // eslint-disable-next-line prefer-const
+// let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
+
+// mongoose.set('debug', true)
+
+// mongoose
+//   .connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('connection established'))
+//   .catch(error => console.log('not connected:', error))
 const mongoose = require('mongoose')
 
-// eslint-disable-next-line prefer-const
-let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
+const username = process.env.MONGODB_USERNAME
+const password = process.env.MONGODB_PASSWORD
+const dbName = process.env.MONGODB_DATABASE
+let connectionString = process.env.MONGODB_CONNECTION_STRING
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.1hrf2s4.mongodb.net/${dbName}?retryWrites=true&w=majority`
+}
 
 mongoose.set('debug', true)
 
@@ -11,4 +31,4 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('connection established'))
-  .catch(error => console.log('not connected:', error))
+  .catch(console.log)
