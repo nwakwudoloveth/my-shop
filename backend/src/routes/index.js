caa27f65f5d3ba72b0b59b2e9e45a41f045bbd78
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2022-12-11 21:24:06 +0000

    added src index file

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: `Coyotiv's Express + MongoDB + Docker + Jest + Vue starter Template` })
+  res.render('index', { title: `Coyotiv's Express + Mongo starter Template` })
 })
 
 module.exports = router
