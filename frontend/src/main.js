ee9432395a82b2476330bef459a54fcd776a09e0
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2022-12-21 11:01:50 +0000

    added icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,23 @@
 import { createApp } from 'vue'
 import App from './app.vue'
 import router from './router'
+//import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
 library.add(faHatWizard)
-library.add(faUserSecret)
+library.add(faMagnifyingGlass)
 
 import 'bootstrap'
 import 'normalize.css'
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
+app.config.productionTip = false
 
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.mount('#app')
