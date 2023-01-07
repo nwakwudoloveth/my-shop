0e7837e302366248138b07d5a67d7a1d235e47ac
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-01-07 18:06:19 +0000

    add counter

---
+++ frontend/src/components/counter.vue
@@ -0,0 +1,20 @@
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'CounterBox',
+  mounted() {
+    this.incrementCount()
+  },
+  methods: {
+    ...mapActions(['incrementCount']),
+  },
+  computed: {
+    ...mapState(['count']),
+  },
+}
+</script>
+
+<template lang="pug">
+div
+    p {{count}}
+</template>
