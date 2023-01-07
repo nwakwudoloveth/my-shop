c96f639f0734e77981b12a11084f649df01c4809
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-01-07 18:16:30 +0000

    add list front end route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import HomeView from '../views/home-view.vue'
+import Login from '../views/login.vue'
+import Register from '../views/register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,43 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/list',
+      name: 'list',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about-view.vue'),
+      component: () => import('../views/list.vue'),
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      beforeEnter(to, from, next) {
+        if (store.state.user) return next('/')
+        return next()
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      beforeEnter(to, from, next) {
+        if (store.state.user) return next('/')
+        return next()
+      },
+    },
+
+    {
+      path: '/products/:id',
+      name: 'Detail',
+      component: () => import('../views/product-detail.vue'),
+    },
+
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: () => import('../views/cart.vue'),
     },
   ],
 })
