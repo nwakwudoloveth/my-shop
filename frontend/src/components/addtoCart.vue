c8175f3505f56b17d4d627ce10c0a15a3c9db714
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-01-07 18:17:12 +0000

    remove cart icon

--- frontend/src/components/addtoCart.vue
+++ frontend/src/components/addtoCart.vue
@@ -6,8 +6,5 @@ export default {
 
 <template lang="pug">
 #app
-  // Add the style and icon you want using the String format
-  h1 hello
-  font-awesome-icon(icon='fa-solid fa-hat-wizard')
-
+  
 </template>
