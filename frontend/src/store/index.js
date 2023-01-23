b758681370cc5c9cba815b3020e0732a0b25c07b
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-01-23 10:48:48 +0000

    Create cart store methods

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -11,6 +11,8 @@ const mutations = {
   INCREMENT_COUNT: 'increment count',
   SET_USER: 'set user',
   SET_PRODUCT: 'set product',
+  SET_CART: 'set cart',
+  REMOVE_ITEM: 'remove item',
 }
 
 //const socket = io(process.env.VITE_APP_BASE_URL)
@@ -20,6 +22,7 @@ export default createStore({
     count: 0,
     user: null,
     products: null,
+    cart: [],
   },
 
   mutations: {
@@ -33,10 +36,21 @@ export default createStore({
     [mutations.SET_PRODUCT](state, products) {
       state.products = products
     },
+    [mutations.SET_CART](state, cart) {
+      state.cart = cart
+    },
+    [mutations.REMOVE_ITEM](state, productId) {
+      state.cart.items = state.cart.items.filter(item => item._id !== productId)
+    },
   },
   actions: {
     incrementCount({ commit }) {
       commit(mutations.INCREMENT_COUNT)
+    },
+    async addToCart({ commit }, productId) {
+      const cartRequest = await axios.post(`/api/cart/items`, { productId })
+      commit(mutations.SET_CART, cartRequest.data)
+      return cartRequest.data
     },
     async fetchUser(store, id) {
       const userRequest = await axios.get(`/api/users/${id}`)
@@ -45,6 +59,17 @@ export default createStore({
     async fetchUsers() {
       const usersRequest = await axios.get('/api/users')
       return usersRequest.data
+    },
+    async fetchCart({ commit }) {
+      const cartItems = await axios.get('/api/cart/items/')
+      commit(mutations.SET_CART, cartItems.data)
+      return cartItems.data
+    },
+    async removeItem({ commit }, productId) {
+      await axios.delete(`/api/cart/items/${productId}`)
+      commit(mutations.REMOVE_ITEM, productId)
+      //return removeRequest.data
+      // location.reload()
     },
     async fetchProduct(store, productId) {
       const productRequest = await axios.get(`/api/products/${productId}`)
