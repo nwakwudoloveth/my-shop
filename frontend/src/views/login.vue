dc53cb026aad2e0f12e27f968e97c4202c50ef6d
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2023-02-02 18:06:56 +0000

    add login error message

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -23,8 +23,8 @@ export default {
         })
 
         this.$router.push('/buyer')
-      } catch (e) {
-        this.backendError = e.response.data.message
+      } catch (error) {
+        this.backendError = error.response.data
       }
     },
   },
@@ -32,36 +32,60 @@ export default {
 </script>
 
 <template lang="pug">
-.login
-    form(@submit="submitLogin")
-      .mb-3
-       h5 Log in to your account
-       label.form-Label(for="email") Email:&nbsp;
-        input.form-control(v-model="email" id="email" type="email" placeholder="Your email" required)
-      .mb-3 
-       label.form-Label(for="password") Password:&nbsp;
-        input.form-control(v-model="password" id="password" type="password" placeholder="Your password" required)
-       input.btn.btn-primary(type="submit" value="Log in")
-      div(v-if="backendError") {{ backendError }}
-      div Don't have an account yet? <router-link to="/register">Register</router-link>
+.container-fluid
+  .row
+    .col-sm-9.col-md-7.col-lg-5.mx-auto
+      .card.border-0.shadow.rounded-3.my-5
+        .card-body.p-4.p-sm-5
+          h5.card-title.text-center.mb-5.fw-light.fs-5 Log In
+          form(@submit="submitLogin")
+            .mb-3
+            
+            label.form-Label(for="email") Email:&nbsp;
+              input.form-control(v-model="email" id="email" type="email" placeholder="Your email" required)
+            .mb-3 
+            label.form-Label(for="password") Password:&nbsp;
+              input.form-control(v-model="password" id="password" type="password" placeholder="Your password" required)
+            div.m-2.error-message(v-show="backendError") Wrong Credentials!! Check your password and e-mail!
+            input.btn.btn-primary.btnclass(type="submit" value="Log in")
+            
+            
+
+            p Don't have an account yet? 
+             |
+             router-link.link(to='/register') Register
+
 </template>
 
 <style lang="scss" scoped>
 label {
   display: block;
-  margin: 1rem 0;
+  margin-bottom: 1rem;
 }
 .login {
   padding: 3% 10% 7%;
   border: 1px;
-  //background-color: orange;
   margin-left: 50px;
   margin-right: 50px;
-  // padding-left: auto;
-  // padding-right: auto;
+}
+.form-control {
+  width: 100%;
+  border-radius: 0px;
 }
 
-.form-control {
-  width: 50%;
+.link {
+  color: #ff8a00;
+}
+.btnclass {
+  height: 44px;
+  width: 130px;
+  margin: auto;
+  color: #ffff;
+  margin-bottom: 5px;
+}
+.error-message {
+  border: solid 1px #ff0000;
+  text-align: center;
+  color: #ff0000;
 }
 </style>
